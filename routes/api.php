71802cc634fcc18a0548518730f82faaa77a5341
Emran <emran.bm@gmail.com>
2016-12-01 16:03:39 +0330

    Sync with vps.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Telegram\Bot\Api;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,35 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('/AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik/webhook', function () {
+
+    $update = Telegram::commandsHandler(true);
+
+    $myfile = fopen("emranfile.txt", "w") or die("Unable to open file!");
+    $txt = $update;
+    fwrite($myfile, $txt);
+    fclose($myfile);
+
+    $telegram = new Api('249465277:AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik');
+
+    $telegram->sendMessage([
+        'chat_id' => $update->get('message')->get('chat')->get('id'),
+        'text' => 'سلام. من زندم. ولی نه اونقدری که جوابتو بدم!'
+    ]);
+
+    return 'ok';
+});
+
+Route::get('/last', function () {
+    $fp = fopen("emranfile.txt", "rb");
+    fseek($fp, 0, SEEK_END);
+    $size = ftell($fp);
+    fclose($fp);
+
+    $myfile = fopen("emranfile.txt", "r") or die("Unable to open file!");
+    $last = fread($myfile, $size);
+    fclose($myfile);
+
+    dd($last);
+});
--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/getWebhook', function(){
+    $telegram = new Api('249465277:AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik');
+    dd($telegram->getWebhookInfo([0]));
+});
+
 Route::get('/setWebhook', function () {
     $telegram = new Api('249465277:AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik');
-    $response = $telegram->setWebhook(['url' => 'https://nasim-trbot/249465277:AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik/webhook']);
+    $response = $telegram->setWebhook(['url' => 'https://nasim-trbot.elenoon.ir/api/AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik/webhook']);
     return $response;
 });
 
-Route::post('/249465277:AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik/webhook', function (Request $request) {
-    error_log($request->all());
+Route::get('/get',function(){
+    $myfile = fopen("emranfile.txt", "w") or die("Unable to open file!");
+    fread($myfile, 20);//filesize("emranfile.txt"));
+    fclose($myfile);
 });
