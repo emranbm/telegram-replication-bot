5d0381ec0c2fb600c61c0cd6614692f972617d16
Emran <emran.bm@gmail.com>
2016-12-01 20:03:48 +0330

    Text transfer

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -22,7 +22,7 @@ class TelegramController extends Controller
         $telegram = new Api('249465277:AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik');
 
         if ($update->get('channel_post') != null)
-            $this->transferMessage($update->get('channel_post')/*->get('chat')->get('id')*/);
+            $this->handleChannelMessage($update->get('channel_post')/*->get('chat')->get('id')*/);
         else
             $telegram->sendMessage([
                 'chat_id' => $update->get('message')->get('chat')->get('id'),
@@ -32,12 +32,42 @@ class TelegramController extends Controller
         return 'ok';
     }
 
-    private function transferMessage($message)
+    private function handleChannelMessage($message)
     {
-        $myfile = fopen("emranfile2.txt", "w") or die("Unable to open file!");
-        $txt = $message;
-        fwrite($myfile, $txt);
-        fclose($myfile);
+//        $myfile = fopen("emranfile2.txt", "w") or die("Unable to open file!");
+//        $txt = $message;
+//        fwrite($myfile, $txt);
+//        fclose($myfile);
 
+        $telegram = new Api('249465277:AAHEzjRDK66dcpRBCodjnDxj8MbsWzl6Cik');
+        $channel = $message->get('chat')->get('username');
+
+        if ($message->get("text") != null) {
+            // so it's a text message
+            $this->postRequest($this->getChannelKey($channel), $message->get('text'));
+        } else if ($message->get('photo') != null) {
+            // it's a photo message
+//            $telegram->getFile()
+        }
+
+    }
+
+    private function postRequest($dest, $text, $mediaAddress = "", $mediaType = "")
+    {
+        $url = 'http://localhost:3000';
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{"destination":"' . $dest . '", "message":"' . $text . '", "mediaAddress":"' . $mediaAddress . '", "thumb":"", "media_type":"' . $mediaType . '"}');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+    }
+
+    private function getChannelKey($channel)
+    {
+        //TODO
+        return 'varzesh3';
     }
 }
